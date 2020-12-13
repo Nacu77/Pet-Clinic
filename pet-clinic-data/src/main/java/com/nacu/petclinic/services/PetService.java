ec587b6f0732d5dfaa93567e0992bcfa7100defa
Nacu Florin <florin.nacu77@gmail.com>
2020-12-13 22:02:07 +0200

    Closes #5.

---
+++ pet-clinic-data/src/main/java/com/nacu/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package com.nacu.petclinic.services;
+
+import com.nacu.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/com/nacu/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.nacu.petclinic.services;
+
+import com.nacu.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+}
