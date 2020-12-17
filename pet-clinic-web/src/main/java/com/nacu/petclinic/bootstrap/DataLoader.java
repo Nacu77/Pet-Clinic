b3a868bb33cd99fdc853a54411498dc4c2f313ad
Nacu Florin <florin.nacu77@gmail.com>
2020-12-17 23:52:52 +0200

    Closes #29.

--- pet-clinic-web/src/main/java/com/nacu/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nacu/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.nacu.petclinic.bootstrap;
 
 import com.nacu.petclinic.model.Owner;
+import com.nacu.petclinic.model.PetType;
 import com.nacu.petclinic.model.Vet;
 import com.nacu.petclinic.services.OwnerService;
+import com.nacu.petclinic.services.PetTypeService;
 import com.nacu.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,29 +15,32 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Loading owners...");
+        PetType dog = new PetType("Dog");
+        PetType cat = new PetType("Cat");
+        PetType savedDogPetType = petTypeService.save(dog);
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner("Florin", "Nacu");
         Owner owner2 = new Owner("Gigi", "Becali");
         Owner owner3 = new Owner("Cristiano", "Ronaldo");
         ownerService.save(owner1);
         ownerService.save(owner2);
         ownerService.save(owner3);
-        System.out.println("Loaded owners successfully");
 
-        System.out.println("Loading vets...");
         Vet vet1 = new Vet("Andreea", "Cristea");
         Vet vet2 = new Vet("Bianca", "Andrei");
         vetService.save(vet1);
         vetService.save(vet2);
-        System.out.println("Loaded vets successfully");
     }
 }
