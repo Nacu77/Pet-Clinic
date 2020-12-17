45e3219b9b2bc782e544d74d66e69283ebe891f3
Nacu Florin <florin.nacu77@gmail.com>
2020-12-17 23:06:29 +0200

    Closes #23.

--- pet-clinic-data/src/main/java/com/nacu/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/nacu/petclinic/model/Owner.java
@@ -1,11 +1,23 @@
 package com.nacu.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
 
     public Owner() {
     }
 
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
