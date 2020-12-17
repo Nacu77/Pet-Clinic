f719f688b70bda0f6031f496930c579fee097df9
Nacu Florin <florin.nacu77@gmail.com>
2020-12-17 23:11:52 +0200

    Closes #24.

--- pet-clinic-data/src/main/java/com/nacu/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/nacu/petclinic/model/Vet.java
@@ -1,11 +1,23 @@
 package com.nacu.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
 
     public Vet() {
     }
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
     }
 }
