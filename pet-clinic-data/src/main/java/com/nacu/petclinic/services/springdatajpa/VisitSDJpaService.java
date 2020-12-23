595a6223445fa759f36be0ad0a50cca026dcd9b5
Nacu Florin <florin.nacu77@gmail.com>
2020-12-23 20:05:41 +0200

    Closes #44.

---
+++ pet-clinic-data/src/main/java/com/nacu/petclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package com.nacu.petclinic.services.springdatajpa;
+
+import com.nacu.petclinic.model.Visit;
+import com.nacu.petclinic.repositories.VisitRepository;
+import com.nacu.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
