4aaab33aefe5f2b9e3039ab505a4282ac05d5613
Nacu Florin <florin.nacu77@gmail.com>
2021-02-16 21:02:10 +0200

    Closes #60.

--- pet-clinic-web/src/main/java/com/nacu/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/nacu/petclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.nacu.petclinic.controllers;
 
+import com.nacu.petclinic.model.Vet;
 import com.nacu.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -16,10 +21,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets.html"})
+    @RequestMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 
 }
